dd7dc52c5663a4e4db5993d02635a3358c1f4e42
yogesh <yogesh.devaliya@gmail.com>
2013-10-17 15:26:14 +0530

    Added DB handle.

---
+++ app/models/db-handle.js
@@ -0,0 +1,16 @@
+var Db = require('mongodb').Db;
+var Connection = require('mongodb').Connection;
+var Server = require('mongodb').Server;
+var BSON = require('mongodb').BSON;
+var ObjectID = require('mongodb').ObjectID;
+var async = require('async');
+DBHandler = function(host, port) {
+  this.db= new Db('improwised', new Server(host, port, {safe: false}, {auto_reconnect: true}, {}));
+  this.db.open(function(error){
+  	if(error){
+  		console.log('Error : ', error);
+  	} else {
+  		console.log('Database connected.');
+  	}
+  });
+};
