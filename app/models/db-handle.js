7f8b30457d8139a4fb13d8bbe36b070bb6d59df9
meet2me <pmeet95@gmail.com>
2013-10-17 16:08:20 +0530

    Method for login added.

--- app/models/db-handle.js
+++ app/models/db-handle.js
@@ -4,28 +4,47 @@ var Server = require('mongodb').Server;
 var BSON = require('mongodb').BSON;
 var ObjectID = require('mongodb').ObjectID;
 
-var dbPort 		= 27017;
-var dbHost 		= 'localhost';
-var dbName 		= 'improwised';
+var dbPort    = 27017;
+var dbHost    = 'localhost';
+var dbName    = 'improwised';
 
 var db = new Db(dbName, new Server(dbHost, dbPort, {auto_reconnect: true}), {w: 1});
-	db.open(function(err, data){
-	if (err) {
-		console.log(err);
-	}	else{
-		console.log('connected to database :: ' + dbName);
-	}
+  db.open(function(err, data){
+  if (err) {
+    console.log(err);
+  } else{
+    console.log('connected to database :: ' + dbName);
+  }
 });
+var accounts = db.collection('accounts');
 
 /* login validation methods */
 
 exports.autoLogin = function(user, pass, callback)
 {
-	accounts.findOne({user:user}, function(err, data) {
-		if (data){
-			data.pass == pass ? callback(data) : callback(null);
-		}	else{
-			callback(null);
-		}
-	});
+  accounts.findOne({user:user}, function(err, data) {
+    if (data){
+      data.pass == pass ? callback(data) : callback(null);
+    } else{
+      callback(null);
+    }
+  });
+}
+
+exports.manualLogin = function(user, pass, callback)
+{
+  accounts.findOne({user:user}, function(err, data) {
+    if (data == null){
+      callback('user-not-found');
+    } 
+    else{
+      validatePassword(pass, data.pass, function(error, res) {
+        if (res){
+          callback(null, data);
+        } else{
+          callback('invalid-password');
+        }
+      });
+    }
+  });
 }
--- app/router.js
+++ app/router.js
@@ -7,60 +7,94 @@ module.exports = function(app) {
 
 // main login page //
 
-	app.get("/", function(req, res){
-		res.render('upload');
-	});
 
-	app.get("/login", function(req, res){
-		res.render("login");
-	});
+  app.get('/', function(req, res){
 
-	app.get("/register", function(req, res){
-		res.render("register");
-	});
+    if (req.cookies.user == undefined || req.cookies.pass == undefined){
+      res.render('login', { title: 'Login' });
+    }
+    else{
+      DB.autoLogin(req.cookies.user, req.cookies.pass, function(data){
+        if (data != null){
+            req.session.user = data;
+          res.redirect('/upload');
+        }
+        else{
+          res.render('login', { title: 'Login' });
+        }
+      });
+    }
+  });
 
-	app.post("/upload", function (request, response){
-	    console.log("Path: "+request.files.fileName.path);
-	    var path = request.files.fileName.path;
-	    var type = request.files.fileName.type;
-	    console.log("Name:"+request.files.fileName.name);
-	    console.log("Type: "+type);
-	    if(type != 'application/zip'){
-	    	response.render("upload");
-	    	console.log("NOT ZIP");
-	    	response.end();
-	    }
-	    else{
-	    	console.log("Its a ZIP");
+  app.post('/', function(req, res){
+    DB.manualLogin(req.param('user'), req.param('pass'), function(err, data){
+      if (!data){
+        res.send(err, 400);
+      } else{
+          req.session.user = data;
+        if (req.param('remember-me') == 'true'){
+          res.cookie('user', data.user, { maxAge: 900000 });
+          res.cookie('pass', data.pass, { maxAge: 900000 });
+        }
+        res.send(data, 200);
+      }
+    });
+  });
 
-	    	fs.createReadStream(request.files.fileName.path).pipe(unzip.Extract({ path: 'output/' }));
-	    	// .on('error', function (err) { console.log('error', err); })
-	    	// .on('close', function () { console.log('closed'); });
+  // app.get('/', function(req, res){
+  //   res.render('upload');
+  // });
 
-	    	var file = getDirectoryName(request.files.fileName.name);
-	    	console.log("File:"+file);
-	    	fs.mkdirsSync('output/user');
-	    	var main = __dirname+'/output';
-	    	console.log(main);
+  app.get('/login', function(req, res){
+    res.render('login');
+  });
 
-	    	var dir = __dirname+'/output/'+file;
-	    	console.log("Dir: "+dir+'/*');
+  app.get('/register', function(req, res){
+    res.render('register');
+  });
 
-	  		//fs.copy(dir+'/', __dirname+'/test', function(err){
-			//   if (err) return console.error(err);
-			//   console.log("success!")
-			// });
+  app.post('/upload', function (request, response){
+      console.log('Path: '+request.files.fileName.path);
+      var path = request.files.fileName.path;
+      var type = request.files.fileName.type;
+      console.log('Name:'+request.files.fileName.name);
+      console.log('Type: '+type);
+      if(type != 'application/zip'){
+        response.render('upload');
+        console.log('NOT ZIP');
+        response.end();
+      }
+      else{
+        console.log('Its a ZIP');
 
-			fs.copySync(dir+'/', __dirname+'/test');
+        fs.createReadStream(request.files.fileName.path).pipe(unzip.Extract({ path: 'output/' }));
+        // .on('error', function (err) { console.log('error', err); })
+        // .on('close', function () { console.log('closed'); });
 
-			// fs.removeSync(dir+'/*');
-			// fs.removeSync(dir);
-	    	response.end();
-	    }
-	});
+        var file = getDirectoryName(request.files.fileName.name);
+        console.log('File:'+file);
+        fs.mkdirsSync('output/user');
+        var main = __dirname+'/output';
+        console.log(main);
 
-	function getDirectoryName(filename) {
-	    var i = filename.lastIndexOf('.');
-	    return (i < 0) ? '' : filename.substr(0,i);
-	}
+        var dir = __dirname+'/output/'+file;
+        console.log('Dir: '+dir+'/*');
+
+        //fs.copy(dir+'/', __dirname+'/test', function(err){
+      //   if (err) return console.error(err);
+      //   console.log('success!')
+      // });
+
+      fs.copySync(dir+'/', __dirname+'/test');
+
+      // fs.removeSync(dir+'/*');
+      // fs.removeSync(dir);
+        response.end();
+      }
+  });
+
+  function getDirectoryName(filename) {
+      var i = filename.lastIndexOf('.');
+      return (i < 0) ? '' : filename.substr(0,i);
+  }
 }
